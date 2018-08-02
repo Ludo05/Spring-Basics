a9dec0a7f68d9f1717e8368734122dd2a631c52a
LewisWilliams <lewiswilliams1501@gmail.com>
2018-08-02 23:49:45 +0100

    Added auto-wiring and reading lists from spring.xml

--- src/main/java/com/lewis/springBasic/Triangle.java
+++ src/main/java/com/lewis/springBasic/Triangle.java
@@ -2,39 +2,59 @@ package com.lewis.springBasic;
 
 import com.lewis.springBasic.Point;
 
+import java.util.List;
+
 public class Triangle {
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
+/*
+    private List<Point> pointList;
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPointList() {
+        return pointList;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
+    public void setPointList(List<Point> pointList) {
+        this.pointList = pointList;
     }
 
-    public Point getPointB() {
-        return pointB;
-    }
+    */
+        public Point getPointA() {
+            return pointA;
+        }
 
-    public void setPointB(Point pointb) {
-        this.pointB = pointb;
-    }
+        public void setPointA(Point pointA) {
+            this.pointA = pointA;
+        }
 
-    public Point getPointC() {
-        return pointC;
-    }
+        public Point getPointB() {
+            return pointB;
+        }
 
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
+        public void setPointB(Point pointb) {
+            this.pointB = pointb;
+        }
+
+        public Point getPointC() {
+            return pointC;
+        }
+
+        public void setPointC(Point pointC) {
+            this.pointC = pointC;
+        }
 
     public void draw(){
+            /*
+        for(Point p : pointList){
+            System.out.println("Point: (" + p.getX() + ", "+p.getY() + ")");
+        }
+        */
         System.out.println("Point A (" + getPointA().getY() + "," + getPointA().getX() + ")");
         System.out.println("Point B (" + getPointB().getY() + "," + getPointB().getX() + ")" );
         System.out.println("Point C (" + getPointC().getY() + "," + getPointC().getX() + ")");
+
+
     }
 }
